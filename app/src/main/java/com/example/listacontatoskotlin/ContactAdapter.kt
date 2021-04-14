9eb323716677b1f95dfb3843d7582ac945186f23
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-04-14 13:46:37 -0300

    implementando métodos getItemCount() e onBindViewHolder()

--- app/src/main/java/com/example/listacontatoskotlin/ContactAdapter.kt
+++ app/src/main/java/com/example/listacontatoskotlin/ContactAdapter.kt
@@ -14,24 +14,34 @@ import androidx.recyclerview.widget.RecyclerView
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
 
     //5 declarando a variavel para lista de contatos
+    //6 - criar a classe modelo Contact
     private val list: MutableList<Contact> = mutableListOf()
 
-
+    // 4 implementar metodos
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
         TODO("Not yet implemented")
     }
 
+    // 4 implementar metodos
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
+    // 4 implementar metodos
+    // lê os itens do array e popula o recyclerview
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //8 - chamando o metodo bind de ContactAdapterViewHolder, passando a posição da lista (objeto Contact)
+        holder.bind(list[position])
     }
 
     //ContactAdapterViewHolder será responsável por gerenciar cada item da lista
     //2 criar o viewholder, primeiro passando a View para ContactAdapterViewHolder e depois passando itemView no construtor do  RecyclerView.ViewHolder
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        //7 - criando o método bind
+        fun bind(contact: Contact) {
+
+        }
 
     }
 
