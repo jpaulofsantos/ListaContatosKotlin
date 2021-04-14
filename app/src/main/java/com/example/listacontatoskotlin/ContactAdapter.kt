a2382d3903bb2f4e4f1873d425f9af6cef1dae1b
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2021-04-14 15:00:48 -0300

    implementando o metodo bind

--- app/src/main/java/com/example/listacontatoskotlin/ContactAdapter.kt
+++ app/src/main/java/com/example/listacontatoskotlin/ContactAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.listacontatoskotlin
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 //contactAdapter será a classe responsável por gerenciar a lista de contatos
@@ -19,7 +21,11 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
 
     // 4 implementar metodos
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        // criação da view
+        //9 criar o layout contact_item e usa-lo no layout inflate
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        //10 retornar o adapterviewholder passando a view
+        return ContactAdapterViewHolder(view)
     }
 
     // 4 implementar metodos
@@ -30,7 +36,7 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     // 4 implementar metodos
     // lê os itens do array e popula o recyclerview
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        //8 - chamando o metodo bind de ContactAdapterViewHolder, passando a posição da lista (objeto Contact)
+        //8 - chamando o metodo bind de ContactAdapterViewHolder, passando a posição da lista (objeto Contact). Populando a view
         holder.bind(list[position])
     }
 
@@ -38,11 +44,16 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     //2 criar o viewholder, primeiro passando a View para ContactAdapterViewHolder e depois passando itemView no construtor do  RecyclerView.ViewHolder
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        //11 - declarando variaveis, buscando pelo IDs atraves do findViewById
+        private val textName: TextView = itemView.findViewById(R.id.textName)
+        private val textPhone: TextView = itemView.findViewById(R.id.textPhone)
+        private val image: ImageView = itemView.findViewById(R.id.picture)
+
         //7 - criando o método bind
         fun bind(contact: Contact) {
-
+            //12 nomeando os campos usando os atributos da classe Contact
+            textName.text = contact.name
+            textPhone.text = contact.phone
         }
-
     }
-
 }
